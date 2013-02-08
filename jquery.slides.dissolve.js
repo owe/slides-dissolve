ebe41a0a600d26892b57dd5f3753d423c080ccc8
Erling Owe <owe@vecora.no>
2013-02-08 23:52:07 +0100

    Updated copyright and license notice

--- jquery.slides.dissolve.js
+++ jquery.slides.dissolve.js
@@ -1,22 +1,11 @@
 /**
- * slides | jQuery plugin
- * Copyright (C) 2010 - 2011 Oktober Media DA
+ * Dissolve transition for slides | jQuery plugin
+ * Copyright (C) 2013 Vecora AS
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * Released under the MIT license and GNU GPLv3
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see http://www.gnu.org/licenses/.
- *
- * @author Erling Owe <owe@oktobermedia.no>
- * @copyright Oktober Media DA, 2010 - 2011
+ * @author Erling Owe <owe@vecora.com>
+ * @copyright Vecora AS, 2013
  */
 
 
